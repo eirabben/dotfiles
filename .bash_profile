5ef04d02d6a4147785b8582286f86140a4de45f5
Eirik Igland <eirabben@gmail.com>
2013-04-04 12:52:03 +0200

    Add script to make symlinks

--- .bash_profile
+++ .bash_profile
@@ -1,3 +1,5 @@
+# Export path for overriding osx defaults
+export PATH="/usr/local/bin:/usr/local/sbin:~/bin:$PATH"
 
 # Load dotfiles
 for file in ~/.{bashrc,bash_prompt,aliases}; do
---
+++ links.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.bash_prompt ~/.bash_prompt
+ln -s ~/.dotfiles/.aliases ~/.aliases
