7aa0da0e45f0d31e858879bd62cbe133573f3172
Eirik Igland <eirabben@gmail.com>
2013-07-28 21:29:50 +0200

    Add tmux conf to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,12 @@
 
 # TODO: copy color schemes and other files.
 
-for file in ~/.{bash_profile,bashrc,vimrc}; do
-
+for file in ~/.{bash_profile,bashrc,vimrc,tmux.conf}; do
     if [ -f $file ]; then
          mv "$file" "${file}_backup" 
     fi
 
     ln -s ~/.dotfiles/$(basename $file) $file
-
 done
 unset file
 
