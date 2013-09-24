d1b1769bfe26fdad1e81e6216fc5b8a675e50871
Eirik Igland <eirabben@gmail.com>
2013-09-24 18:37:07 +0200

    Add file copying to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # TODO: Copy color schemes for terminals, vim and tmux
-# TODO: Add vundle to vim
 # TODO: Make update mode for bootstrap.sh
 
+# Add vundle to vim folder
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+# Copy vim color scheme to vim folder
+cp ~/.dotfiles/vim/base16-railscasts.vim ~/.vim/colors/
+
+# Create symlinks
 for file in ~/.{bash_profile,bashrc,vimrc,tmux.conf}; do
     if [ -f $file ]; then
          mv "$file" "${file}_backup" 
