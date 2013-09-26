0154a725af3c9dc4739a618ce423110412baebc4
Eirik Igland <eirabben@gmail.com>
2013-09-26 12:47:58 +0200

    add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Remove files made by installer
+rm $HOME/.bash_profile
+rm $HOME/.bashrc
+rm $HOME/.vimrc
+rm $HOME/.tmux.conf
+rm -r $HOME/.vim
+
+# Move original files back to home directory
+mv $HOME/.dotfiles/backup/* $HOME/
+
