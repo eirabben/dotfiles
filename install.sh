770d6109b3803647fd6c7aaebbdb7beb6a874d99
Eirik Igland <eirabben@gmail.com>
2013-09-26 11:21:47 +0200

    add automatic xfce terminal theming

--- install.sh
+++ install.sh
@@ -21,3 +21,11 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 # Install bundles
 vim +BundleInstall +qall
 
+# If xfce-terminal is installed, link to color scheme
+if [ -d "$HOME/.config/xfce/terminal" ]; then
+    if [ -f "$HOME/.config/xfce/terminal/terminalrc" ]; then
+        mv $HOME/.config/xfce/terminal/terminalrc $HOME/.config/xfce/terminal/terminalrc_backup
+        ln -s $HOME/.dotfiles/base16/base16-xfce4-terminal-master/base16-railscasts.dark.terminalrc $HOME/.config/xfce/terminal/terminalrc
+    fi
+fi
+
