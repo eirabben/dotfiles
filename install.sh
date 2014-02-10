70c3fd9d11fe4032513268a8a7eaad9ebcd46726
Eirik Igland <eirabben@gmail.com>
2014-02-10 17:24:56 +0100

    fix mv statement and vundle install in install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Create symlinks, move backups to backup directory
 for file in $HOME/.{bash_profile,bashrc,vimrc,tmux.conf,gitconfig,vim}; do
     if [ -f $file ]; then
-         mv -r "$file" "$HOME/.dotfiles/backup/" 
+         mv "$file" "$HOME/.dotfiles/backup/" 
     fi
 
     ln -s $HOME/.dotfiles/$(basename $file) $file
@@ -11,6 +11,8 @@ done
 unset file
 
 # Add vundle to vim folder
-git clone https://github.com/gmarik/vundle.git ~/.dotfiles/.vim/bundle/vundle
+if [ ! -d $HOME/.dotfiles/.vim/bundle/vundle ]; then
+    git clone https://github.com/gmarik/vundle.git ~/.dotfiles/.vim/bundle/vundle
+fi
 # Install bundles
 vim +BundleInstall +qall
