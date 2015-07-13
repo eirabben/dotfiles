449a478597ec0095a548a7bac8cc17865e0ff79d
Eirik Igland <eirabben@gmail.com>
2015-07-13 16:49:36 +0200

    Add vundle submodule

---
+++ symlink.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+home="$HOME"
+dotfiles="$HOME/.dotfiles"
+
+if [[ -d "$dotfiles" ]]; then
+  echo "Symlinking dotfiles from $dotfiles"
+else
+  echo "$dotfiles does not exist"
+  exit 1
+fi
+
+for file in .bash_profile .bashrc .gitconfig .tmux.conf .vim .vimrc; do
+	echo "\tLinking $file"
+	ln -s ~/.dotfiles/$file ~/$file
+done
+
+source ~/.bashrc
