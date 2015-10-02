cc339f08fbc1d9969969e5ccaa1fdb3d64a23cd1
Eirik Igland <eirabben@gmail.com>
2015-10-02 12:31:09 +0200

    Add Vundle as submodule

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 #/bin/bash
+
+DOTFILES_ROOT=$(pwd)
+
+for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink')
+do
+  dst="$HOME/.$(basename "${src%.*}")"
+  ln -nfs "$src" "$dst"
+  echo " - File $dst linked."
+done
