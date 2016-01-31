8e0ec83d4dae19099689f359c252cae5ef646b1d
Eirik Igland <eirabben@gmail.com>
2016-01-31 19:00:18 +0100

    Add some git info to prompt

---
+++ config.symlink/fish/config.fish
@@ -0,0 +1,20 @@
+alias vim "nvim"
+
+
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+#set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'auto'
+#set __fish_git_prompt_color_branch yellow
+#set __fish_git_prompt_color_upstream_ahead green
+#set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '*' # File has been modified
+set __fish_git_prompt_char_stagedstate '+' # File is staged
+set __fish_git_prompt_char_untrackedfiles '?' # Untracked file
+#set __fish_git_prompt_char_stashstate ''
+#set __fish_git_prompt_char_upstream_ahead '^'
+#set __fish_git_prompt_char_upstream_behind 'v'
