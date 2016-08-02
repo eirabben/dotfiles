872b956000f94ee27bfc1842db827ab83abc7542
Eirik Igland <eirabben@gmail.com>
2016-08-02 11:40:21 +0200

    Make scripts executable

--- script/fish.sh
+++ script/fish.sh
@@ -3,7 +3,7 @@
 function generate_fish_config {
   print_action "Generating Fish config"
   generate_file "$dotfiles_dir/fish/config.fish.template" "$home_dir/.config/fish"
-  generate_file "$dotfiles_dir/fish/functions/fish_prompt.fish.template" "$home_dir/.config/fish/functions"
+  generate_file "$dotfiles_dir/fish/fish_prompt.fish.template" "$home_dir/.config/fish/functions"
   print_ok
 }
 
--- script/tmux.sh
+++ script/tmux.sh
@@ -2,7 +2,7 @@
 
 function generate_tmux_conf {
   print_action "Generating tmux config"
-  generate_file "$dotfiles_dir/tmux/tmux.conf.template" "$home_dir"
+  generate_file "$dotfiles_dir/tmux/.tmux.conf.template" "$home_dir"
   print_ok
 }
 
