f11fcab0e41d2e8dc7066f6c271c10da0434dc95
Eirik Igland <eirabben@gmail.com>
2021-12-18 18:34:15 +0100

    change to auto theme for lualine

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -5,7 +5,7 @@ end
 
 lualine.setup({
 	options = {
-		theme = "rose-pine",
+		theme = "auto",
 		-- component_separators = { left = '>', right = '<'},
 		-- section_separators = { left = '', right = ''},
 		disabled_filetypes = { "NvimTree" },
