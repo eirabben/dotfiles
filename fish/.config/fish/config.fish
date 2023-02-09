43e8068becf4a5688b7983b520b14df595e1e74a
Eirik Igland <eirabben@gmail.com>
2023-02-09 08:55:50 +0100

    feat: Combine vi and emacs keybinds for Fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,7 +9,16 @@ fish_add_path "/opt/homebrew/opt/mysql-client/bin"
 # Customization
 #################################################
 
-fish_vi_key_bindings
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
+
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
+end
 starship init fish | source
 
 #################################################
