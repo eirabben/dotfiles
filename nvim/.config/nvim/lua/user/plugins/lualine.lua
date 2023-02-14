f7adeed5b0683405f9f779981d11730ba54c760e
Eirik Igland <eirabben@gmail.com>
2023-02-14 12:12:47 +0100

    feat: Change lualine config

--- nvim/.config/nvim/lua/user/plugins/lualine.lua
+++ nvim/.config/nvim/lua/user/plugins/lualine.lua
@@ -1,22 +1,21 @@
---[[ TODO: Customize config ]]
-
 require("lualine").setup({
 	options = {
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = {
 				"NvimTree",
+				"NeoTree",
 			},
 		},
 	},
 	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
+		lualine_a = {},
+		lualine_b = { "mode" },
+		lualine_c = { { "filename", path = 1 } },
+		lualine_x = { "encoding", "filetype", "progress" },
+		lualine_y = { "location" },
+		lualine_z = {},
 	},
 	inactive_sections = {
 		lualine_a = {},
@@ -55,5 +54,4 @@ require("lualine").setup({
 --[[ 	component_separators = "|", ]]
 --[[ 	section_separators = "", ]]
 --[[ }, ]]
-
 -- Old config
