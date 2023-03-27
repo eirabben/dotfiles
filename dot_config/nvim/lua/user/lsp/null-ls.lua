1f4368d8dd8114e26735a98510597d9334b50aea
Eirik Igland <eirabben@gmail.com>
2023-03-27 10:39:41 +0200

    feat: Add autoformat via null-ls

--- dot_config/nvim/lua/user/lsp/null-ls.lua
+++ dot_config/nvim/lua/user/lsp/null-ls.lua
@@ -1,8 +1,46 @@
 local null_ls = require("null-ls")
 
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.diagnostics.twigcs,
+		null_ls.builtins.formatting.rustywind,
+		null_ls.builtins.formatting.rustywind.with({
+			filetypes = {
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+				"vue",
+				"svelte",
+				"html",
+				"twig",
+			},
+		}),
 	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					lsp_formatting(bufnr)
+				end,
+			})
+		end
+	end,
 })
