ea48cd76dd4fbc987f2e4ae40018fc03fd335847
Eirik Igland <eirabben@gmail.com>
2023-05-05 10:30:55 +0200

    chore(nvim): Remove twigcs

--- dot_config/nvim/lua/user/lsp/null-ls.lua
+++ dot_config/nvim/lua/user/lsp/null-ls.lua
@@ -16,7 +16,6 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.prettierd,
-		null_ls.builtins.diagnostics.twigcs,
 		null_ls.builtins.formatting.rustywind,
 		null_ls.builtins.formatting.rustywind.with({
 			filetypes = {
