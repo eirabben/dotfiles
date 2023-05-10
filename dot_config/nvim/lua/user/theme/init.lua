1a43d3c07029ca8b6d45d22471958c6681c04e24
Eirik Igland <eirabben@gmail.com>
2023-05-10 22:13:14 +0200

    feat(nvim): Change colorscheme

--- dot_config/nvim/lua/user/theme/init.lua
+++ dot_config/nvim/lua/user/theme/init.lua
@@ -5,5 +5,6 @@ require("nightfox").setup({})
 require("rose-pine").setup({})
 require("kanagawa").setup({})
 
-vim.cmd.colorscheme("kanagawa-wave")
+-- vim.cmd.colorscheme("kanagawa-wave")
 -- vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("rose-pine-moon")
