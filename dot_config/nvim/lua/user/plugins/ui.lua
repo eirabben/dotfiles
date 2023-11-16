b48d7da22dab269f4855b257a6f3f9ad89aec72a
Eirik Igland <eirabben@gmail.com>
2023-11-16 08:37:57 +0100

    feat: Add dashboard plugin

--- dot_config/nvim/lua/user/plugins/ui.lua
+++ dot_config/nvim/lua/user/plugins/ui.lua
@@ -1,4 +1,15 @@
 return {
+	-- Dashboard
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+			require("dashboard").setup({
+				-- config
+			})
+		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	},
 	-- Indent lines
 	{
 		"lukas-reineke/indent-blankline.nvim",
@@ -27,57 +38,57 @@ return {
 	},
 
 	-- UI Components
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		-- dependencies = {
-		--   -- which key integration
-		--   {
-		--     "folke/which-key.nvim",
-		--     opts = function(_, opts)
-		--       if require("lazyvim.util").has("noice.nvim") then
-		--         opts.defaults["<leader>sn"] = { name = "+noice" }
-		--       end
-		--     end,
-		--   },
-		-- },
-		opts = {
-			lsp = {
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-				},
-			},
-			routes = {
-				{
-					filter = {
-						event = "msg_show",
-						find = "%d+L, %d+B",
-					},
-					view = "mini",
-				},
-			},
-			presets = {
-				bottom_search = true,
-				command_palette = true,
-				long_message_to_split = true,
-				inc_rename = true,
-			},
-		},
-		-- keys = {
-		--   { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-		--   { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-		--   { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
-		--   { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
-		--   { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
-		--   { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
-		--   { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
-		-- },
-	},
+	-- {
+	-- 	"folke/noice.nvim",
+	-- 	event = "VeryLazy",
+	-- 	-- dependencies = {
+	-- 	--   -- which key integration
+	-- 	--   {
+	-- 	--     "folke/which-key.nvim",
+	-- 	--     opts = function(_, opts)
+	-- 	--       if require("lazyvim.util").has("noice.nvim") then
+	-- 	--         opts.defaults["<leader>sn"] = { name = "+noice" }
+	-- 	--       end
+	-- 	--     end,
+	-- 	--   },
+	-- 	-- },
+	-- 	opts = {
+	-- 		lsp = {
+	-- 			override = {
+	-- 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+	-- 				["vim.lsp.util.stylize_markdown"] = true,
+	-- 			},
+	-- 		},
+	-- 		routes = {
+	-- 			{
+	-- 				filter = {
+	-- 					event = "msg_show",
+	-- 					find = "%d+L, %d+B",
+	-- 				},
+	-- 				view = "mini",
+	-- 			},
+	-- 		},
+	-- 		presets = {
+	-- 			bottom_search = true,
+	-- 			command_palette = true,
+	-- 			long_message_to_split = true,
+	-- 			inc_rename = true,
+	-- 		},
+	-- 	},
+	-- 	-- keys = {
+	-- 	--   { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+	-- 	--   { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+	-- 	--   { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+	-- 	--   { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+	-- 	--   { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+	-- 	--   { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
+	-- 	--   { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
+	-- 	-- },
+	-- },
 
 	{
 		"stevearc/dressing.nvim",
-		lazy = true,
+		event = "VeryLazy",
 		init = function()
 			---@diagnostic disable-next-line: duplicate-set-field
 			vim.ui.select = function(...)
