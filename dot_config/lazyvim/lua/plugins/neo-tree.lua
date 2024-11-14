1a7fe79e3339ce93bef68345f86a21efb86a28c8
Eirik Igland <eirabben@gmail.com>
2024-11-14 19:56:01 +0100

    feat(lvim): Stop neo-tree from following the current file

--- dot_config/lazyvim/lua/plugins/neo-tree.lua
+++ dot_config/lazyvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,14 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      filesystem = {
+        follow_current_file = { enabled = false },
+        window = {
+          mappings = {
+            ["/"] = nil,
+          },
+        },
+      },
       window = {
         mappings = {
           ["e"] = "open",
