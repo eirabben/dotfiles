40bacb64104562367d5966b330b2ed52b33d2273
Eirik Igland <eirabben@gmail.com>
2025-04-26 20:58:55 +0200

    feat(lvim): Add example colorscheme file

---
+++ dot_config/lazyvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+-- return {
+--   {
+--     "rebelot/kanagawa.nvim",
+--   },
+--   { "rose-pine/neovim", name = "rose-pine" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "rose-pine-moon",
+--     },
+--   },
+-- }
