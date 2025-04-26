f1de2bad6a3ed5c7bb16434adadbf3f10cd80ff1
Eirik Igland <eirabben@gmail.com>
2025-04-26 21:29:05 +0200

    feat(lvim): Enable json and md extras

--- dot_config/lazyvim/lua/config/lazy.lua
+++ dot_config/lazyvim/lua/config/lazy.lua
@@ -24,6 +24,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.php" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.mini-comment" },
     { import = "plugins" },
